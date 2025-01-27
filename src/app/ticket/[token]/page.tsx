092c381a2d54fda7d96d17b80882dc5a864015c2
kisia0916 <fumiharuabe@gmail.com>
2025-01-27 23:03:12 +0900

    set type

--- src/app/ticket/[token]/page.tsx
+++ src/app/ticket/[token]/page.tsx
@@ -4,7 +4,7 @@ import TokenQR from '@/components/TokenQR/TokenQR'
 import jwt from "jsonwebtoken"
 
 
-async function page({params}: {params: {token: string}}) {
+async function page({params}: {params:Promise<{token:string}>}) {
   const {token} = await params
   let display_flg:boolean = true
   let qr_text:string = ""
